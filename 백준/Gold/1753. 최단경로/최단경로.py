98293c4c9176319be4e35bd5099dbb865f2fbbd1
최용수 <37789623+TutiTuti@users.noreply.github.com>
2023-10-21 17:58:34 +0900

    [Gold IV] Title: 최단경로, Time: 476 ms, Memory: 135256 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,43 +1,39 @@
-import sys, heapq
+# 터치 X
+import sys;
+import heapq
 input = sys.stdin.readline
 
-INF = int(1e9)                     # 무한 의미(10억)
+INF = int(1e9)
 V, E = map(int, input().split())
 start = int(input())
-graph = [[] for _ in range(V+1)]   # 노드의 정보를 담을 리스트
-distance = [INF] * (V+1)           # 최단 거리 테이블 무한으로 초기화
+adj = [[] for _ in range(V+1)]
+dis = [INF] * (V+1)
+# vstd = [0] * (V+1)
 
-# 간선 정보 입력 받기
+
 for _ in range(E):
-    a, b, c = map(int, input().split())  # a번 노드에서 b번 노드로 가는 비용 c
-    graph[a].append((b,c))
+    s, e, w = map(int, input().split())
+    adj[s].append((e, w))
 
-
-def dijkstra(start):
+def find(start):
     Q = []
-    heapq.heappush(Q, (0, start))  # 시작노드로 가기 위한 최단경로를 0하여 Q에 삽입
-    distance[start] = 0
-
+    heapq.heappush(Q, (0, start))
+    dis[start] = 0
     while Q:
-        cur_cost, now = heapq.heappop(Q)  # 현재 최단 거리가 가장 짧은 노드 정보 꺼내기
+        now_c, now_n = heapq.heappop(Q)
 
-        if distance[now] < cur_cost:      # 계산된 거리보다 현재 거리가 더 크면 볼 필요X
+        if dis[now_n] < now_c:
             continue
 
-        for next, next_cost in graph[now]:       # b번 노드, 비용 c
-            cost = cur_cost + next_cost          # 현재 거리 + 다음에 갈 거리
+        for next_n, next_c in adj[now_n]:
+            cost = now_c + next_c
+            if cost < dis[next_n]:
+                dis[next_n] = cost
+                heapq.heappush(Q, (cost, next_n))
+find(start)
 
-            if cost < distance[next]:            # 현재 노드를 거쳐서 이동하는게 더 짧은 경우
-                distance[next] = cost            # 최단 거리 갱신
-                heapq.heappush(Q, (cost, next))  # 비용 기준으로 최소힙에 삽입
-
-
-dijkstra(start)
-
-
-# 모든 노드로 가기 위한 최단 거리 출력
 for i in range(1, V+1):
-    if distance[i] == INF:
+    if dis[i] == INF:
         print('INF')
     else:
-        print(distance[i])
+        print(dis[i])
