ae0fb0efb25011fb6d84eece002e082a607b45a7
최용수 <37789623+TutiTuti@users.noreply.github.com>
2023-10-25 23:19:17 +0900

    [Gold IV] Title: 네트워크 연결, Time: 440 ms, Memory: 59880 KB -BaekjoonHub

---
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,27 @@
+import sys;
+input = sys.stdin.readline
+import heapq
+def prim(s):
+    c=0
+    tmpans = 0
+    Q = [[0, s]]
+    vstd = [0]*(V+1)
+    while c != V:
+        weight, s = heapq.heappop(Q)
+        if vstd[s]:
+            continue
+        vstd[s] = 1
+        tmpans += weight
+        c += 1
+        for i in arr[s]:
+            heapq.heappush(Q, i)
+    return tmpans
+
+V = int(input())
+E = int(input())
+arr = [[] for _ in range(V+1)]
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    arr[a].append([c, b])
+    arr[b].append([c, a])
+print(prim(1))
