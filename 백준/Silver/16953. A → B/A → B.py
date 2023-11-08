be9194854a301769ba9386317a69564df0be0921
최용수 <37789623+TutiTuti@users.noreply.github.com>
2023-11-08 20:15:35 +0900

    [Silver II] Title: A → B, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.py
+++ 백준/Silver/16953. A → B/A → B.py
@@ -1,17 +1,16 @@
-def find(n, v):
-    global ans
-
-    if v > M:              # M 보다 커지거나 0보다 작아진다면 return
-        return
-    elif v == M:                     # 값을 찾으면 ans 갱신
-        ans = n
-        return
+A, B = map(int, input().split())
+ans = -1
+cnt = 1
+while B >= A:
+    if A == B:
+        ans = cnt
+        break
+    elif str(B)[-1] == '1':
+        B = int(str(B)[:len(str(B))-1])
+        cnt += 1
+    elif B % 2 == 1:
+        break
     else:
-        find(n+1, v*2)               # 2배
-        find(n+1, int(str(v)+'1'))   # 끝에 1추가 해준다.
-
-
-N, M = map(int, input().split())
-ans = -1                             # 값을 못 찾으면 -1이 출력 된다.
-find(1, N)
+        B //= 2
+        cnt += 1
 print(ans)
