96baa14448658f000976c06891490236b17a69c9
최용수 <37789623+TutiTuti@users.noreply.github.com>
2024-01-20 20:49:50 +0900

    [Gold V] Title: 빗물, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.py
@@ -0,0 +1,34 @@
+# 실패코드 2
+H, W = map(int, input().split())
+arr=list(map(int, input().split()))
+left=0
+ans=0
+tmp=0
+for i in range(W):
+    if arr[i] >= arr[left] or i == W-1: # arr[i]가 왼쪽 벽가 같거나 클 때 or 마지막 인덱스 일 때
+        # print("Y")
+        if arr[i]==0: # 여기 이상한데
+            # print("첫번째가 or 마지막이 0일 때")
+     
+            continue
+        # print("min :", min(arr[i], arr[left]), i-left-1, tmp, min(arr[i], arr[left])*(i-left-1)-tmp)
+        if i:
+            ans += min(arr[i], arr[left])*(i-left-1)-tmp
+        left=i
+        tmp=0
+    elif arr[i] < arr[left]: # left i보다 크다면 ..  >> 벽 작은 arr[i]가 나머지 중에 가장 큰 값일 수도 있음.
+        # print("N")
+        flag=False
+        for j in range(i+1,W):
+            # print("find big num",arr[i] ,j , arr[j])
+            if arr[j] > arr[i]:
+                flag=True
+                break
+        if flag:
+            tmp += arr[i]
+            # print("tmp :", tmp)
+        else:
+            ans += min(arr[i], arr[left])*(i-left-1)-tmp
+            left=i
+            tmp=0
+print(ans)
