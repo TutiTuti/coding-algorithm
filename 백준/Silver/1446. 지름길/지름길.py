bd254c92690720bf4c1ef322e32d47a7a157e42e
최용수 <37789623+TutiTuti@users.noreply.github.com>
2025-05-09 13:41:36 +0900

    [Silver I] Title: 지름길, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.py
+++ 백준/Silver/1446. 지름길/지름길.py
@@ -1,10 +1,14 @@
+
 N, D = map(int, input().split())
-distance = [i for i in range(D+1)]
+dist = [i for i in range(D+1)]
 shortcuts = [list(map(int, input().split())) for _ in range(N)]
 
 for i in range(D+1):
-    distance[i] = min(distance[i], distance[i-1]+1)
-    for s, e, shortcut in shortcuts:
-        if i == s and e <= D and distance[e] > distance[i] + shortcut:
-            distance[e] = distance[i] + shortcut
-print(distance[D])
+    dist[i] = min(dist[i], dist[i-1]+1)
+
+    for s, f, l in shortcuts:
+        if i == s and f <= D:
+            d =dist[i] + l
+            if dist[f] > d:
+                dist[f] = dist[i] + l
+print(dist[D])
